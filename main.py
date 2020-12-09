fdacba5953ec7074c143b53357bed5482d9e2472
Dmitry <dimk00z@gmail.com>
2020-12-09 15:56:24 +0300

    add send message

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 import asyncio
 import gui
-from utils import get_parser, open_connection, get_answer, write_line_to_file, load_from_file
 from time import time
+from utils import get_parser, open_connection, \
+    get_answer, write_line_to_file, load_from_file
 
 
-async def write_chat_line_to_file(chat_file_name, chat_line):
-    async with aiofiles.open(chat_file_name, "a") as chat_history:
-        await chat_history.write(chat_line)
-
-
-async def read_msgs(host, port, queue, histoty_queue, file_name, attempts=3):
+async def read_msgs(host, port,
+                    queue, histoty_queue,
+                    file_name, attempts=3):
     await load_from_file(file_name, message_queue=queue)
     async with open_connection(host, port, attempts) as rw:
         reader = rw[0]
@@ -24,17 +22,27 @@ async def save_messages(histoty_queue, file_name):
     while True:
         message = await histoty_queue.get()
         await write_line_to_file(chat_file_name=file_name, line=message)
-# async def generate_msgs(queue):
-#     while True:
-#         message = f'Ping {int(time())}'
-#         queue.put_nowait(message)
-#         await asyncio.sleep(1)
+
+
+async def send_msgs(host, port, queue):
+    while True:
+        msg = await queue.get()
+        print(msg)
+    # async def generate_msgs(queue):
+    #     while True:
+    #         message = f'Ping {int(time())}'
+    #         queue.put_nowait(message)
+    #         await asyncio.sleep(1)
 
 
 async def main():
 
     parser = get_parser()
     args = parser.parse_args()
+    host = args.host
+    attempts = args.attempts
+    port = args.port
+    history_file_name = args.file_name
     print(args)
 
     messages_queue = asyncio.Queue()
@@ -42,15 +50,6 @@ async def main():
     status_updates_queue = asyncio.Queue()
     histoty_queue = asyncio.Queue()
 
-    # messages_queue.put_nowait('Привет обитателям чата!')
-    # messages_queue.put_nowait('Как дела?')
-    # status_updates_queue.put_nowait(gui.ReadConnectionStateChanged.CLOSED)
-    # event = gui.NicknameReceived('Василий Пупкин')
-    # status_updates_queue.put_nowait(event)
-    host = args.host
-    attempts = args.attempts
-    port = args.port
-    history_file_name = args.file_name
     await asyncio.gather(
         read_msgs(host=host, port=port,
                   queue=messages_queue,
@@ -61,7 +60,8 @@ async def main():
                  sending_queue=sending_queue,
                  status_updates_queue=status_updates_queue),
         save_messages(histoty_queue=histoty_queue,
-                      file_name=history_file_name))
+                      file_name=history_file_name),
+        send_msgs(host, port, sending_queue))
 
 
 if __name__ == '__main__':
